68a2721bcf7e704ea7cfaf344bf341d5c0fd53c6
Devin Peck <devinpeck@icloud.com>
2017-05-10 23:46:39 -0300

    changed UI
    added edit text and button. textview updates on button click based on
    edit text

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,10 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button);
+        final EditText input = (EditText) findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                TextView text = (TextView) findViewById(R.id.helloText);
+                text.setText(input.getText());
+            }
+        });
+
     }
+
 }
